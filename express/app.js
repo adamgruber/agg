72644743982d21f5181fb158ba175be5eb3d54e1
Adam Gruber <adamg@aweber.com>
2019-07-09 13:16:41 -0400

    remove other routes

--- express/app.js
+++ express/app.js
@@ -46,45 +46,45 @@ app.use('/templates', express.static(path.join(__dirname, 'lib', 'views', 'templ
 // app.use(router);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    if (err && err.status === 404) {
-      res.status(404);
-      res.render('404', config.mainPage);
-    } else {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-    }
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     if (err && err.status === 404) {
+//       res.status(404);
+//       res.render('404', config.mainPage);
+//     } else {
+//       res.status(err.status || 500);
+//       res.render('error', {
+//         message: err.message,
+//         error: err
+//       });
+//     }
+//   });
+// }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  if (err && err.status === 404) {
-    res.status(404);
-    res.render('404', config.mainPage);
-  } else {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {}
-    });
-  }
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   if (err && err.status === 404) {
+//     res.status(404);
+//     res.render('404', config.mainPage);
+//   } else {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: {}
+//     });
+//   }
+// });
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
